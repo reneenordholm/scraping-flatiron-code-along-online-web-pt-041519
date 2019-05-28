54eb0eb30aab469c52d2615a3cc22ed30b064a25
Renee Nordholm <reneenordholm@gmail.com>
2019-05-28 21:43:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,8 +16,9 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
+            binding.pry
   end 
-      binding.pry
+
   
     def print_courses
     self.make_courses
