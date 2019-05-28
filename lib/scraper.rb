65955614be853d248b9a2e794f803fdfa09b321d
Renee Nordholm <reneenordholm@gmail.com>
2019-05-28 21:41:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,6 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
-    doc.css(".post")
   end
   
     def print_courses
